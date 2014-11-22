cca562a515db5b03b09cec1f4e88dea76aeef57e
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-11-22 15:39:34 +0000

    Added WTFEngine, nested JSON, changed method

---
+++ js/wtf_main.js
@@ -0,0 +1,41 @@
+
+$(function() {
+
+    // Populate using an object literal
+
+    // WTF.init({
+
+    //     heading: [
+    //         "It's a fucking",
+    //         "Check this shit out, a fucking"
+    //     ],
+    //     response: [
+    //         "Already fucking seen one",
+    //         "Give me a-fucking-nother one",
+    //         "Don't really give a fuck"
+    //     ],
+    //     template: [
+    //         "Big @color @animal",
+    //         "Silly @animal with @color fur"
+    //     ],
+    //     animal: [
+    //         "dog",
+    //         "cat",
+    //         "rabbit"
+    //     ],
+    //     color: [
+    //         "red",
+    //         "blue",
+    //         "green",
+    //         "yellow"
+    //     ]
+    // });
+
+    // Populate using a JSON file
+    WTF.init( '/js/sample.json' );
+
+    // Populate using a Google spreadsheet ID (you must publish it first!)
+    // @see https://support.google.com/drive/answer/37579?hl=en
+    // WTF.init( '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc' );
+
+});
